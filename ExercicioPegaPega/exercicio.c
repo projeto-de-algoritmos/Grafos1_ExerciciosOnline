ef32dd0019a06da21bbaea9794c920a843798f08
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2023-09-20 18:37:30 -0300

    Criação de DFS

--- ExercicioPegaPega/exercicio.c
+++ ExercicioPegaPega/exercicio.c
@@ -14,6 +14,7 @@ typedef struct Aresta
 typedef struct itemLista{
     struct itemLista *next; 
     int vertice; //vértice v
+    int visitado;
 }item;
 
 typedef item *link; //Link é um ponteiro para nó
@@ -32,19 +33,23 @@ Grafo *InicializaGrafo(int v);                //Função que inicializa o grafo
 void InsereArestaGrafo(Grafo *G, Aresta e);   //Função que insere aresta no grafo
 void ImprimeGrafo(Grafo *G);
 
+void DFS_visit(Grafo *G, link t);
+void DFS (Grafo *G);
+
 int main(){
     
-    int qtdVertices;
-    scanf("%d", &qtdVertices);
+    int qtdVertices, posicaoMenino;
+    scanf("%d %d ", &qtdVertices, &posicaoMenino);
     Grafo *G = InicializaGrafo(qtdVertices+1);
-    scanf("%d");
 
-    for(int i =0; i< qtdVertices;i++){
+    for(int i = 1; i< qtdVertices; i++){
         int v, w;
         scanf("%d %d", &v, &w);
         InsereArestaGrafo(G, RetornaAresta(v, w));
     }
-    ImprimeGrafo(G);
+
+    DFS(G);
+    // ImprimeGrafo(G);
     return 0;
 }
 
@@ -79,6 +84,9 @@ Grafo *InicializaGrafo(int v){
 
     for (v = 0; v < G->V; v++){
         G->adj[v] = NULL;
+        for(link t = G->adj[v]; t != NULL; t = t->next){
+            t->visitado = 0;
+        }
     }
 
     return G;
@@ -91,6 +99,12 @@ void InsereArestaGrafo(Grafo *G, Aresta e){
     G->adj[v] = Enfileira(w, G->adj[v]);
     G->adj[w] = Enfileira(v, G->adj[w]);
 
+    link t = G->adj[v];
+    t->visitado = 0;
+
+    t = G->adj[w];
+    t->visitado = 0;
+
     G->E++;
 }
 
@@ -101,9 +115,28 @@ void ImprimeGrafo(Grafo *G){
     for (v = 0; v < G->V; v++){
         printf("[%d] --> ", v);
         for(t = G->adj[v]; t != NULL; t = t->next){
-            printf("%d --> ", t->vertice);
+            printf("%d(%d) --> ", t->vertice, t->visitado);
         }
         printf("NULL\n");
     }
     
+}
+
+void DFS_visit(Grafo *G, link t){
+    t->visitado = 1;
+    for(link w = G->adj[t->vertice]; w != NULL; w = w->next){
+        if(w->visitado!=1){         
+            DFS_visit(G, w);
+        }
+    }
+}
+
+void DFS (Grafo *G){
+    for (int v = 0; v < G->V; v++){
+        for(link t = G->adj[v]; t != NULL; t = t->next){
+            if(t->visitado!=1){         
+                DFS_visit(G, t);
+            }
+        }
+    }
 }
