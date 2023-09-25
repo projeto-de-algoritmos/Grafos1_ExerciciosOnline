916a12f1c72bc5c8b12dce473d29c50b4fed21a0
Lucas Spinosa <lucasspinosa.software@gmail.com>
2023-09-25 12:10:42 -0300

    Reorganização do código

--- ExercicioIrEVir/exercicioIrEVir.c
+++ ExercicioIrEVir/exercicioIrEVir.c
@@ -1,82 +1,77 @@
 #include<stdio.h>
 #include <stdbool.h>
 
-
-bool graph[2001][2001];
+bool cidade[2001][2001];
 bool visitados[2001];
 int N, P;
  
-void profundidade( int ponto ){
- if( visitados[ ponto ] == true ){
-    return;
- }else{
-    visitados[ ponto ] = true;
-    for( int i = 1; i <= N; i++ )
-     if( graph[ ponto ][ i ] && !visitados[ i ] )
-      profundidade( i );
- }
-}
+void DFS( int vertice );
  
 int main(){
  
     scanf(" %d", &N);
+
     while( N != 0 ){
+
         scanf(" %d", &P);
-         for( int i = 1; i <= N; i++ ){
+
+        for( int i = 1; i <= N; i++ ){
             visitados[ i ] = false;
-               for( int j = 1; j <= N; j++ ){
-                   graph[ i ][ j ] = false;
-                }
-         }
+
+            for( int j = 1; j <= N; j++ ){
+                cidade[ i ][ j ] = false;
+            }
+        }
  
         for( int i = 1; i <= P; i++ ){
-            int c, d, sel;
-            scanf(" %d %d %d", &c, &d, &sel);
-         
-            if( sel == 1 ){
-                graph[ c ][ d ] = true; 
+
+            int vertice_a, vertice_b, direcao;
+
+            scanf(" %d %d %d", &vertice_a, &vertice_b, &direcao);
+
+            if( direcao == 1 ){
+                cidade[ vertice_a ][ vertice_b ] = true; 
             }
-             
+
             else{
-                graph[ c ][ d ] = true;
-                graph[ d ][ c ] = true;
-                }
+                cidade[ vertice_a ][ vertice_b ] = true; 
+                cidade[ vertice_b ][ vertice_a ] = true; 
+            }
+
         }
  
-        profundidade( 1 );
+        DFS( 1 );
  
-        bool solucao = true;
+        bool conectado = true;
         for( int i = 1; i <= N; i++ ){
             if( !visitados[ i ] ){
-                solucao = false;
+                conectado = false;
             }
         }
  
-        if( solucao == true ){
+        if( conectado == true ){
             for( int i = 1; i <= N; i++ ){
                 visitados[ i ] = false;
                 for( int j = i+1; j <= N; j++ ){
-                    if( graph[ i ][ j ] && graph[ j ][ i ] )
-                      continue;
-                    else if( graph[ i ][ j ] ){
-                        graph[ j ][ i ] = true; graph[ i ][ j ] = false;
-                    }
-                    else if( graph[ j ][ i ] ){
-                        graph[ i ][ j ] = true; graph[ j ][ i ] = false;
+                    if( cidade[ i ][ j ] && cidade[ j ][ i ] ) continue;
+                    else if( cidade[ i ][ j ] ){
+                        cidade[ j ][ i ] = true; cidade[ i ][ j ] = false;
+                    }else if( cidade[ j ][ i ] ){
+                        cidade[ i ][ j ] = true; cidade[ j ][ i ] = false;
                     }
                 }
              }
  
-            profundidade( 1 );
+            DFS( 1 );
         }
 
         for( int i = 1; i <= N; i++ ){
             if( !visitados[ i ] ){
-                solucao = false;
+                conectado = false;
             }
         }
  
-        if( solucao ){
+        if( conectado ){
            printf("1\n"); 
         } 
         else {
@@ -86,4 +81,17 @@ int main(){
     }
  
     return 0;
+}
+
+void DFS( int vertice ){
+    if( visitados[ vertice ] == true ){
+        return;
+    }
+
+    else{
+        visitados[ vertice ] = true;
+
+    for( int i = 1; i <= N; i++ )
+        if( cidade[ vertice ][ i ] && !visitados[ i ] ) DFS( i );
+    }
 }
